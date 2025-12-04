62254c4942f9d71ec4d1c1ad7135aeaa78465f2b
nchampiondev <nicolas.champion.dev@gmail.com>
2025-12-04 14:50:50 +0100

    day 1 part 2 done

--- 2025/day_1.py
+++ 2025/day_1.py
@@ -6,8 +6,13 @@ A rotation starts with an L or R which indicates whether
 Turning the dial left from 0 one click makes it point at 99
 Turning the dial right from 99 one click makes it point at 0
 The dial starts by pointing at 50
+Part 1:
 Password is the number of times the dial is left pointing at 0 
     after any rotation in the sequence
+
+Part 2:
+Password is the number of times any click causes the dial to point at 0, 
+    regardless of whether it happens during a rotation or at the end of one
 """
 
 from __future__ import annotations
@@ -46,21 +51,26 @@ class Dial:
 
     def turnLeft(self):
         self.current = self.current.prev
+        return self.isPointingAtZero()
 
     def turnRight(self):
         self.current = self.current.next
+        return self.isPointingAtZero()
 
     def turn(self, rotation: str):
+        counter = 0
         direction = rotation[0]
         distance = int(rotation[1:])
 
         if direction == "L":
             for _ in range(distance):
-                self.turnLeft()
+                counter += 1 if self.turnLeft() else 0
 
         if direction == "R":
             for _ in range(distance):
-                self.turnRight()
+                counter += 1 if self.turnRight() else 0
+
+        return counter
 
     def isPointingAtZero(self):
         return self.current.position == 0
@@ -72,12 +82,9 @@ def main():
     with open("day_1_input.txt", mode="r", encoding="utf-8") as file:
         for line in file:
             rotation = line.strip()
-            dial.turn(rotation)
-            if dial.isPointingAtZero():
-                password += 1
+            password += dial.turn(rotation)
 
     print(password)
 
 
 main()
-
