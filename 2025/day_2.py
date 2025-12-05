7ad1b64baffb4bce9d4743c7c0486f955d0a8c27
nchampiondev <nicolas.champion.dev@gmail.com>
2025-12-05 13:40:18 +0100

    day 2 part 2 done

--- 2025/day_2.py
+++ 2025/day_2.py
@@ -1,6 +1,6 @@
 """
 find the invalid IDs by looking for any ID 
-    which is made only of some sequence of digits repeated twice. 
+    which is made only of some sequence of digits repeated twice (part 1). 
 So, 55 (5 twice), 
     6464 (64 twice), and 
     123123 (123 twice) would all be invalid IDs.
@@ -8,9 +8,9 @@ None of the numbers have leading zeroes; 0101 isn't an ID at all. 101 is a valid
 Your job is to find all of the invalid IDs that appear in the given ranges.
 
 What do you get if you add up all of the invalid IDs ?
-"""
 
-# split the str in half and check if both str equal
+(part2 : sequence of digits repeated AT LEAST twice)
+"""
 
 from __future__ import annotations
 
@@ -18,6 +18,15 @@ class IdRange:
     def __init__(self, start: int, end: int):
         self.start = start
         self.end = end
+
+def isInvalidId(i: int):
+    id_str = str(i)
+    str_length = len(id_str)
+    for rep in range(2, str_length+1):
+        rep_length = str_length // rep
+        if id_str[:rep_length] * rep == id_str:
+            return True
+    return False
 
 def main():
     with open("day_2_input.txt", mode="r", encoding="utf-8") as file:
@@ -28,11 +37,7 @@ def main():
         tmp = id_range.split("-")
         r = IdRange(int(tmp[0]), int(tmp[1]))
         for i in range(r.start, r.end+1):
-            s = str(i)
-            mid = len(s) // 2
-            left = s[:mid]
-            right = s[mid:]
-            if left == right:
+            if isInvalidId(i):
                 result += i
     
     print(result)
